1e10bad277e224177ffe8ea17005123a5b90a19c
kuroneko <kuroneko@njust.edu.cn>
2026-05-20 06:53:32 -0400

    520同步

--- src/standard_mpc_fyt.cpp
+++ src/standard_mpc_fyt.cpp
@@ -24,7 +24,7 @@
 
 const std::string keys =
   "{help h usage ? | | 输出命令行参数说明}"
-  "{@config-path   | configs/infantry3.yaml | yaml配置文件路径 }";
+  "{@config-path   | configs/infantry.yaml | yaml配置文件路径 }";
 
 using namespace std::chrono_literals;
 
